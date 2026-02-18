44229c76b31f7153e9229fed6c57b988c3415e9d
Yacine Fodil <105779815+yfodil@users.noreply.github.com>
2026-02-18 17:50:45 +0100

    feat(edge_services): add support for pipeline identity (#3679)

--- provider/sdkv2_test.go
+++ provider/sdkv2_test.go
@@ -170,7 +170,6 @@ func TestSDKProvider_ResourceIdentityNotEmpty(t *testing.T) {
 		"scaleway_edge_services_backend_stage",
 		"scaleway_edge_services_dns_stage",
 		"scaleway_edge_services_head_stage",
-		"scaleway_edge_services_pipeline",
 		"scaleway_edge_services_route_stage",
 		"scaleway_edge_services_tls_stage",
 		"scaleway_edge_services_waf_stage",
